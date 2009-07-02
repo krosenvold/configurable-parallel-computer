31be8837c015c472b448161b9c17ff128a1ff68b
Kristian Rosenvo1d <t535255@tna-06-5053.corp.telenor.no>
2009-07-02 13:47:32 +0200

    Updated license headers

--- src/main/java/org/junit/experimental/ConcurrentRunnerInterceptorBase.java
+++ src/main/java/org/junit/experimental/ConcurrentRunnerInterceptorBase.java
@@ -1,15 +1,30 @@
+/*
+ * Copyright 2002-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Also licensed under CPL http://junit.sourceforge.net/cpl-v10.html
+ */
+
+
 package org.junit.experimental;
 
 import java.util.concurrent.Future;
 import java.util.concurrent.ExecutionException;
 import java.util.List;
 
-/**
- * Created by IntelliJ IDEA.
- * User: kristian
- * Date: Jun 30, 2009
- * Time: 6:54:00 PM
- * To change this template use File | Settings | File Templates.
+/*
+ * @author Kristian Rosenvold, kristianAzeniorD0Tno
  */
 public abstract class ConcurrentRunnerInterceptorBase {
     public abstract void done() throws InterruptedException, ExecutionException;
--- src/main/java/org/junit/experimental/ConfigurableParallelComputer.java
+++ src/main/java/org/junit/experimental/ConfigurableParallelComputer.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright 2002-2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Also licensed under CPL http://junit.sourceforge.net/cpl-v10.html
+ */
+
+
+
 package org.junit.experimental;
 
 import java.util.concurrent.ExecutorService;
@@ -11,6 +31,9 @@ import org.junit.runners.model.InitializationError;
 import org.junit.runners.model.RunnerBuilder;
 import org.junit.runners.model.RunnerInterceptor;
 
+/*
+ * @author Kristian Rosenvold, kristianAzeniorD0Tno
+ */
 public class ConfigurableParallelComputer extends Computer {
     private final boolean fClasses;
     private final boolean fMethods;
